732b265d54e22437c0f80808fc0ab6b2247343e5
Jagdeep Singh <jagdeep.iosdeveloper@gmail.com>
2018-10-23 04:47:01 -0700

    responsive done

---
+++ JS/aloha.js
@@ -0,0 +1,16 @@
+console.log("Just Checking");
+// document.getElementsByClagessName("Spring_summer_head").innerHTML = "Changed" ;
+// document.getElementById("Spring_summer_head").innerHTML = "Changed"
+
+// $ (function() {
+//     var add = $("button"),
+//     $list = $(".my-list");
+//     $list.on("click", "delete", function(event) {
+    
+//   });
+//     add.on("click",function(){
+//       var text =  $("#new-item").attr('value');
+//       var newValue = <li><span>text</span><a href="#" class="delete">[done]</a></li>;
+//   $("$list").append(newValue);
+//     });
+//   });
