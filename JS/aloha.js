a1071c56902ae768a563ca9f50b32b342b5b96dd
Jagdeep Singh <jagdeep.iosdeveloper@gmail.com>
2018-10-29 13:29:48 -0700

    Final_commit

--- JS/aloha.js
+++ JS/aloha.js
@@ -1,96 +1,117 @@
+// nav bar variable
+var nav = $("#_header");
 
-    
- $('.counter').hide()
+
+
+//    hide counter div on cart
+$(".counter").hide();
+
 
 
 // carousel
 
-$(document).ready(function(){
-   
-    var interval = window.setInterval(rotateSlides, 3000)
-    
-    function rotateSlides(){
-        $("#carousel ul").animate({marginLeft:-480},1000,function(){
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
-      }
-    })
-
-    $('.main-carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
-      });
-
-        // Email Validation 
-
-        let emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        
-        
-      $("#subscribeButton").click(function(){
-        let email  = $('#emailTextField').val();
-        var emailVail = emailReg.test(email);
-        if(emailVail) { 
-           alert("You have subscribed sucessfully.")
-           $('#emailTextField').val('')
-        } else {
-            // Swal('Oops...', 'Something went wrong!', 'error')
-            alert("Please check your email")
-        }
-    });
-    
-    $( document ).ready(function() {    
-        var counter = 0
-       
-        $('.cartButton').click(function() {
-            $('.counter').show();
-           counter += 1
-           $('.counter').text(counter)
-        });
-    });
-    
- 
-$(document).on('click', 'a', function(e) {
-    // target element id
-    var id = $(this).attr('href');
-    console.log(id);
-    // target element
-   
-    
-        $('html, body').animate({
-                    scrollTop: $(id).offset().top - 70
-                }, 1000); 
-    
-
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
-
-   
-});
-    $( document ).ready(function() {    
-    var prev = 0;
-    var $window = $(window);
-    var nav = $('#_header');
-
-    $window.on('scroll', function(){
-    // console.log("scrolling")
-   
-    var scrollTop = $window.scrollTop();
+$(document).ready(function() {
   
-    // console.log(scrollTop)
-    if  (scrollTop > prev) {
-      if (scrollTop > 110) {
-        nav.hide();
-      }
-     
-    //   console.log("true")
-    //   console.log(nav)
-  }else {
-    nav.show();
-    // console.log("false")  
+
+  let interval = window.setInterval(rotateSlides, 3000);
+
+  function rotateSlides() {
+    $("#carousel ul").animate({ marginLeft: -480 }, 1000, function() {
+      $(this)
+        .find("li:last")
+        .after($(this).find("li:first"));
+      $(this).css({ marginLeft: 0 });
+    });
   }
-  prev = scrollTop;
-//   console.log(prev)
+
+  $(".main-carousel").flickity({
+    // options
+    cellAlign: "left",
+    contain: true
+  });
 });
- });
+
+// Email Validation
+
+let emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+$("#subscribeButton").click(function() {
+  let email = $("#emailTextField").val();
+  let emailVail = emailReg.test(email);
+
+  if (email === "") {
+    // alert("Please enter your email.");
+    Swal("Please enter your email.");
+  } else if (emailVail) {
+    swal({
+      type: "success",
+      title: "You have subscribed sucessfully",
+      showConfirmButton: false,
+      timer: 1500
+    });
+
+    $("#emailTextField").val("");
+  } else {
+    Swal("Oops...", "Please check your email", "error");
+    // alert("")
+  }
+});
+
+//------------------------------- Add to Cart Function
+
+var counter = 0;
+
+$(".cartButton").click(function() {
+  $(".counter").show();
+  counter += 1;
+  $(".counter").text(counter);
+  swal({
+    type: "success",
+    title: "Item added to cart sucessfully",
+    showConfirmButton: false,
+    timer: 1500
+  });
+});
+
+// --------------------------------Smooth Scrolling
+
+$(document).on("click", "a", function(e) {
+  // target element id
+  var id = $(this).attr("href");
+  console.log(id);
+  // target element
+  $("html, body").animate(
+    {
+      scrollTop: $(id).offset().top - 70
+    },
+    1000
+  );
+  
+  e.preventDefault();
+});
+
+//   --------------------------------Manage_header_along_with_scrolling_
+// var prev = 0;
+// var $window = $(window);
+
+
+// $window.on("scroll", function() {
+//   // console.log("scrolling")
+
+//   var scrollTop = $window.scrollTop();
+
+//   // console.log(scrollTop)
+//   if (scrollTop > prev) {
+//     if (scrollTop > 250) {
+//       nav.hide();
+//     }
+
+//     //   console.log("true")
+   
+//   } else {
+//     nav.show();
+//     // console.log("false")
+//   }
+//   prev = scrollTop;
+//   //   console.log(prev)
+// });
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-This is  Aloha Website includes HTML/CSS/jQuery. I have used CSS Box Model , Flex , Grid , Lists, Background images, font-face , Font-Awesome, Responsive Designs. and the most amazing jQuery.
+This is  Aloha Website includes HTML/CSS/jQuery. I have used CSS Box Model , Flex, Lists, anchor tags, Background images, font-face , Font-Awesome, Responsive Designs. and the most amazing jQuery.
  I learned a lot.
